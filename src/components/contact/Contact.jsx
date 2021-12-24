f31fcb1867ddc47acead8719a10ead0c29cbcf96
mehdi fadaei <m_mehdi_000@yahoo.com>
2021-12-24 23:28:49 +0330

    finish sry for this bad commits :(

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,30 @@
 import "./contact.scss"
+import {useState} from 'react'
 
 function Contact(props) {
+    const [message, setMessage] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setMessage(true);
+    };
     return (
-        <div className={'contact'} id={'contact'}>
-            asdas
+        <div className="contact" id="contact">
+            <div className="left">
+                <img src="assets/shake.svg" alt="" />
+            </div>
+            <div className="right">
+                <h2>Contact.</h2>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder="Email" />
+                    <textarea placeholder="Message"></textarea>
+                    <button type="submit">Send</button>
+                    {message && <span>Thanks, I'll reply ASAP :)</span>}
+                </form>
+                <a href="#intro">
+                    <img src="assets/down.png" className={'downToUp'} alt="" />
+                </a>
+            </div>
         </div>
     );
 }
